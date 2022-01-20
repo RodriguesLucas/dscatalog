ee0d8de75409f767e4074b4c7546dcf41a75d2e1
Lucas Rodrigues <lucasrodrigued84@gmail.com>
2022-01-19 22:00:08 -0300

    first commit

---
+++ backend/src/main/java/com/project/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,26 @@
+package com.project.dscatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.dscatalog.entities.CategoryEntity;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	@Autowired
+	
+	@GetMapping
+	public ResponseEntity<List<CategoryEntity>> findAll(){
+		List<CategoryEntity> list = new ArrayList<>();
+		list.add(new CategoryEntity(1L, "Books"));
+		return ResponseEntity.ok().body(list);
+	}
+}
+ 
