0e8df6b58d94414b525b275be5b538814a6c38ea
Lucas Rodrigues <lucas.rodrigues@compasso.com.br>
2022-01-21 08:16:38 -0300

    Database seeding

--- backend/src/main/java/com/project/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/project/dscatalog/services/CategoryService.java
@@ -2,8 +2,10 @@ package com.project.dscatalog.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.dscatalog.entities.CategoryEntity;
 import com.project.dscatalog.repositories.CategoryRepository;
@@ -12,7 +14,8 @@ import com.project.dscatalog.repositories.CategoryRepository;
 public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
-
+	
+	@Transactional(readOnly = true)
 	public List<CategoryEntity> findAll() {
 		return categoryRepository.findAll();
 	}
