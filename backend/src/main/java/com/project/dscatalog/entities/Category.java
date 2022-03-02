8c639cede0c1015bbda54765899c3e30c1d5e905
Lucas Rodrigues <lucas.rodrigues@compasso.com.br>
2022-03-02 14:31:21 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/project/dscatalog/entities/Category.java
+++ backend/src/main/java/com/project/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.project.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -27,6 +30,9 @@ public class Category implements Serializable {
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") // UTC
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -58,6 +64,10 @@ public class Category implements Serializable {
 
 	public Instant getUpdatedAt() {
 		return updatedAt;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@PrePersist
@@ -92,7 +102,5 @@ public class Category implements Serializable {
 		return "CategoryEntity [id=" + id + ", name=" + name + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt
 				+ "]";
 	}
-	
-	
 
 }
